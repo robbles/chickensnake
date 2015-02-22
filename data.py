7969ab7f76224f15e3f35e7d3e820e2fe6aeb686
Rob O'Dwyer <odwyerrob@gmail.com>
2015-02-21 17:43:20 -0800

    fix bug with https version

--- data.py
+++ data.py
@@ -26,9 +26,9 @@ def manhattan_dist(pos1, pos2):
     return (abs(pos1[0] - pos2[0]) + abs(pos1[1] - pos2[1]))
 
 def get_snake(snakes):
-    url = url_for('base', _external=True).rstrip('/')
+    url = url_for('base', _external=True).rstrip('/').lstrip('https://').lstrip('http://')
     app.logger.debug('Snake URL should be %s', url)
 
     for snake in snakes:
-        if snake['url'].rstrip('/') == url:
+        if snake['url'].rstrip('/').lstrip('https://').lstrip('http://') == url:
             return snake
